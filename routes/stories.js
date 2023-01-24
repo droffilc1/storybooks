2f357db463e4d3b9139d8f4a62cbc3ca548506c9
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-01-24 09:53:39 +0300

    Method override for DELETE requests

--- routes/stories.js
+++ routes/stories.js
@@ -93,4 +93,26 @@ router.put('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('errors/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      await Story.remove({ _id: req.params.id })
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('errors/500')
+  }
+})
+
 module.exports = router
