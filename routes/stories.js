020faacdd3d1cdea983b8f56dce60c8e1ac11adc
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-01-24 09:24:55 +0300

    Edit stories

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,32 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc Show edit page
+// @route GET /stories/edit:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean()
+
+    if (!story) {
+      return res.render('errors/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      res.render('stories/edit', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('errors/500')
+  }
+})
+
+
 
 
 
